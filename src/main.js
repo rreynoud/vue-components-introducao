89d3fabdb943c5e786498a54393378b4bedbdde9
Rafael <rreynoud@gmail.com>
2019-08-19 19:28:35 -0300

    criado novo component

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import Vue from 'vue'
+import { constants } from 'zlib';
+
+
 
 Vue.component('rre-component', {
   data() {
     return {
-      titulo: 'Curso de vueJs'
+      titulo: 'Curso de vueJs',
+      contador: 0
     }
   },
-  template: '<h1>{{titulo}}</h1>'
+  methods: {
+    incrementar : function(){
+      this.contador++
+    }
+  }, 
+  template: `
+    <div>
+    <h1>{{titulo}}</h1>
+    <button @click="incrementar" >Clicado {{contador}} vezes </button> 
+    </div>
+    `
 })
 
 new Vue({
