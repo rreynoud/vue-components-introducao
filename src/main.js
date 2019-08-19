37fb78c8014813e07f928a3d7d0035339c0e23d3
Rafael <rreynoud@gmail.com>
2019-08-19 19:19:19 -0300

    Funcionando o h1 do componente

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 
+Vue.component('rre-component', {
+  data() {
+    return {
+      titulo: 'Curso de vueJs'
+    }
+  },
+  template: '<h1>{{titulo}}</h1>'
+})
+
 new Vue({
-  el:'#app',
-  template: '<h1>Vue components!</h1>'
+  el:'#app'
 })
